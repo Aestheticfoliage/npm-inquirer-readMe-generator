64a5a6627cebb5b22d9add9466572e2b3ea4700b
dewey preast <webwalkin@me.com>
2022-05-01 15:34:03 -0400

    beginning to work on the readme for this project to get the layout for the prompts

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
+// these commands include the inquirer package and tells it to look in the file system as well ('fs')
+const inquirer = require("inquirer");
+const fs = require("fs");
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
 const questions = [];
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'title',
+      message: 'what is the title of your project',
+    },{
+        type:
+    }
+    {
+        type: 'checkbox',
+        name: 'languages used in making this project',
+        choices:['HTML', 'CSS','Java','Javascript',
+    ]
+    }
+  ])
+  .then((data) => {
+    console.log(data.name);
+  });
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
